4aeaf2354dfab7a9c0744a99f09685dad04e9c90
nlisker <37422899+nlisker@users.noreply.github.com>
2022-06-08 03:13:31 +0300

    Remove websockets

--- src/main/java/com/gpsdemo/WebSocketView.java
+++ src/main/java/com/gpsdemo/WebSocketView.java
@@ -15,18 +15,18 @@ public class WebSocketView extends View {
 	static TextArea textArea = new TextArea();
 
 	public WebSocketView() {
-		URI uri = null;
-		try {
-			uri = new URI("ws://localhost:8887");
-		} catch (URISyntaxException e1) {
-			e1.printStackTrace();
-		}
-		WebSocketClient client = new Connection(uri);
-		client.connect();
-		Button send = new Button("Send");
-		send.setOnAction(e -> client.send("Sending"));
-
-		setCenter(textArea);
-		setBottom(send);
+//		URI uri = null;
+//		try {
+//			uri = new URI("ws://localhost:8887");
+//		} catch (URISyntaxException e1) {
+//			e1.printStackTrace();
+//		}
+//		WebSocketClient client = new Connection(uri);
+//		client.connect();
+//		Button send = new Button("Send");
+//		send.setOnAction(e -> client.send("Sending"));
+//
+//		setCenter(textArea);
+//		setBottom(send);
 	}
 }
